e4b48aaf2978667617826373e2504ec3b0bfa4c6
Thomas Scheltema <scheltemat@yahoo.com>
2022-08-09 12:01:26 -0500

    more layout

---
+++ scripts/functions.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+            
+
+          
+    $('.items').slick({
+infinite: true,
+lazyLoad: 'ondemand',
+slidesToShow: 3,
+slidesToScroll: 3
+});
+
+
+  
+
+
+
+   });
