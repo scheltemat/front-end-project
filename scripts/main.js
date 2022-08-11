e157ba6f38c3925c77dda5745a86ac59f2614251
RKovl <ryan.kovlaske@gmail.com>
2022-08-11 15:05:36 -0700

    buttons work

--- scripts/main.js
+++ scripts/main.js
@@ -1,99 +1,103 @@
 let printCard = document.querySelector(".row")
 let printImg = document.querySelectorAll("img")
 
-const placesFetch = async (location)=>{
-    let results = await fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=food&location=${location}&radius=3000&type=restaurant&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry%2Cphotos&key=AIzaSyDs76VeBs3S5YCAJfu_KkiepYooAYIX12k`)
-    if (!results.ok) {
-        throw new Error(`An error occurred: ${results.status}`);
-    }
+// const placesFetch = async (location)=>{
+//     let results = await fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=food&location=${location}&radius=3000&type=restaurant&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry%2Cphotos&key=AIzaSyDs76VeBs3S5YCAJfu_KkiepYooAYIX12k`)
+//     if (!results.ok) {
+//         throw new Error(`An error occurred: ${results.status}`);
+//     }
     
-    return await results.json();
+//     return await results.json();
 
-}
-// `https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=food&location=49.8880%2C-119.4960&radius=1000&type=restaurant&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry%2Cphotos&key=AIzaSyDs76VeBs3S5YCAJfu_KkiepYooAYIX12k`
+// }
+// // `https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=food&location=49.8880%2C-119.4960&radius=1000&type=restaurant&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry%2Cphotos&key=AIzaSyDs76VeBs3S5YCAJfu_KkiepYooAYIX12k`
 
-//business_status,name,opening hours: periods/weekday_text,photos,rating,geometry
+// //business_status,name,opening hours: periods/weekday_text,photos,rating,geometry
 
-const photosFetch = async (ref)=>{
-   // console.log("f1")
-   for(let C in ref){
-      let results = await fetch(`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${ref[C]}&key=AIzaSyDs76VeBs3S5YCAJfu_KkiepYooAYIX12k`)
-         if (!results.ok) {
-            throw new Error(`An error occurred: ${results.status}`);
-      }
-   }
-   //  console.log(results)
-   //  return results.json();
+// const photosFetch = async (ref)=>{
+//    // console.log("f1")
+//    for(let C in ref){
+//       let results = await fetch(`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${ref[C]}&key=AIzaSyDs76VeBs3S5YCAJfu_KkiepYooAYIX12k`)
+//          if (!results.ok) {
+//             throw new Error(`An error occurred: ${results.status}`);
+//       }
+//    }
+//    //  console.log(results)
+//    //  return results.json();
 
-}
+// }
 
-const loadFetch = async ()=>{
-    const placesParent = await placesFetch("49.8880%2C-119.4960")
-    let placesData = placesParent.results
-    let hold = {} //i can make it gobal the reset it on fuctions start if needed
-    let photosHold = []
-    console.log(placesData)
-    for(let A = 0; A<placesData.length;A++){
-        hold[A] = placesData[A]
+// const loadFetch = async ()=>{
+//     const placesParent = await placesFetch("49.8880%2C-119.4960")
+//     let placesData = placesParent.results
+//     let hold = {} //i can make it gobal the reset it on fuctions start if needed
+//     let photosHold = []
+//     console.log(placesData)
+//     for(let A = 0; A<placesData.length;A++){
+//         hold[A] = placesData[A]
         
-    }
-   for(let B in hold){
-      console.log(B)
-      let currentHold = hold[B]
-      console.log(currentHold.rating)
-      if (currentHold.rating>4.5){
-         console.log(currentHold.photos[0].photo_reference)
-         photosHold.push(currentHold.photos[0].photo_reference)
-         console.log("reak here")
-         // let photosData = await photosFetch(photosHold)
-         // console.log(photosData)
-         let eachCard =`
-         <div class="col" id="item${B}">
-               <div class="card h-100">
-                  <img src="" class="card-img-top" alt="...">
-                  <div class="card-body">
-                     <h5 class="card-title">${currentHold.name}</h5>
-                     <p class="card-text">${currentHold.rating} /n ${currentHold.business_status}</p>
-                  </div>
-               </div>
-               </div>`
-         printCard.innerHTML += eachCard
-            // photosData
-            // currentHold.business_status
-            // currentHold.geometry.location.lat
-            // currentHold.geometry.location.lng
-            // currentHold.name
-            // currentHold.rating
-         }
-   }
-   const photosArr = await Promise.all(
-      photosHold.map(async photoRef =>{
-         let photoReponse = await fetch(`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${photoRef}&key=AIzaSyDs76VeBs3S5YCAJfu_KkiepYooAYIX12k`)
-         return photoReponse.url
-      })
-   )
-   let printImg = document.querySelectorAll("img")
-   console.log(photosArr)
-   for(let C = 0; C<photosArr.length;C++){
-      printImg[C].src = photosArr[C]
-   }
+//     }
+//    for(let B in hold){
+//       console.log(B)
+//       let currentHold = hold[B]
+//       console.log(currentHold.rating)
+//       if (currentHold.rating>4.5){
+//          console.log(currentHold.photos[0].photo_reference)
+//          photosHold.push(currentHold.photos[0].photo_reference)
+//          console.log("reak here")
+//          // let photosData = await photosFetch(photosHold)
+//          // console.log(photosData)
+//          let eachCard =`
+//          <div class="col">
+//                <div class="card h-100">
+//                   <img src="" class="card-img-top" alt="...">
+//                   <div class="card-body">
+//                      <h5 class="card-title">${currentHold.name}</h5>
+//                      <p class="card-text">${currentHold.rating} /n ${currentHold.business_status}</p>
+//                   </div>
+//                </div>
+//                </div>`
+//          printCard.innerHTML += eachCard
+//             // photosData
+//             // currentHold.business_status
+//             // currentHold.geometry.location.lat
+//             // currentHold.geometry.location.lng
+//             // currentHold.name
+//             // currentHold.rating
+//          }
+//    }
+//    const photosArr = await Promise.all(
+//       photosHold.map(async photoRef =>{
+//          let photoReponse = await fetch(`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${photoRef}&key=AIzaSyDs76VeBs3S5YCAJfu_KkiepYooAYIX12k`)
+//          return photoReponse.url
+//       })
+//    )
+//    let printImg = document.querySelectorAll("img")
+//    console.log(photosArr)
+//    for(let C = 0; C<photosArr.length;C++){
+//       printImg[C].src = photosArr[C]
+//    }
 
-   //  console.log("hihi")
-   //  console.log(printImg[0])
-   //  printImg[0].src = await photosFetch()
-   //  console.log(printImg[0].src)
-   //  console.log("ahhasd")
-   //  for(let C in photosHold) {
-   //    let photosData = await photosFetch(photosHold[C])
-   //    printImg[C].src = 
-   //  }
+//    //  console.log("hihi")
+//    //  console.log(printImg[0])
+//    //  printImg[0].src = await photosFetch()
+//    //  console.log(printImg[0].src)
+//    //  console.log("ahhasd")
+//    //  for(let C in photosHold) {
+//    //    let photosData = await photosFetch(photosHold[C])
+//    //    printImg[C].src = 
+//    //  }
     
-}
+// }
 
 let restaurantButton = document.getElementById('restaurant')
 restaurantButton.addEventListener('click', e =>{
-   loadFetch();
-   restaurantButton.disabled = true;
+   let printCard = document.querySelector(".row")
+    printCard.style.display = "flex"
+    let weathercard = document.querySelector('#weather2')
+    weathercard.style.display = "none"
+
+   // restaurantButton.disabled = true;
 })
 
 // console.log(placeFetch()) 
