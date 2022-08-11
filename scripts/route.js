a11d01f2e53d76607d939068711a5ecaec6f1236
RKovl <ryan.kovlaske@gmail.com>
2022-08-11 11:09:56 -0700

    food places no show up in click

--- scripts/route.js
+++ scripts/route.js
@@ -12,6 +12,8 @@ endID.value
 let weatherAd = ""
 let placeAd = []
 
+var service;
+
 
 function initMap(){
   const map = new google.maps.Map(
@@ -47,13 +49,80 @@ function initMap(){
     );
     }
   };
+
+  const service = new google.maps.places.PlacesService(map);
+
+
   google.maps.event.addListener(map, "click", (event) => {
-    addMarker(event.latLng, map);
-    placeAd = event.latLng
-    console.log(event.latLng)
-    console.log(placeAd)
+    service.nearbySearch(
+      { location: event.latLng, radius: 1000, type: "restaurant" },
+      (results, status,) => {
+        if (status !== "OK" || !results) return;
+        addPlaces(results, map);
+      });
+
+    // const request = {
+    //   query: "restaurant",
+    //   fields: ["name", "geometry","rating"],
+    //   locationBias: event.latLng
+    // };
+
+    // service = new google.maps.places.PlacesService(map);
+
+    // service.findPlaceFromQuery(request, function(results, status) {
+    //   if (status === google.maps.places.PlacesServiceStatus.OK) {
+    //     for (var i = 0; i < results.length; i++) {
+    //       createMarker(results[i]);
+    //     }
+    //     map.setCenter(results[0].geometry.location);
+    //   }
+    // });
+  
+    // addMarker(event.latLng, map);
+    // placeAd = event.latLng
+    // console.log(event.latLng)
+    // console.log(placeAd)
   });  
 }
+function addPlaces(places, map) {
+  // const placesList = document.getElementById("places");
+
+  for (const place of places) {
+    if (place.geometry && place.geometry.location) {
+      const image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+      };
+
+      new google.maps.Marker({
+        map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location,
+      });
+
+      // const li = document.createElement("li");
+
+      // li.textContent = place.name;
+      // placesList.appendChild(li);
+      // li.addEventListener("click", () => {
+      //   map.setCenter(place.geometry.location);
+      // });
+    }
+  }
+}
+
+// function createMarker(place = google.maps.places.PlaceResult) {
+//   if (!place.geometry || !place.geometry.location) return;
+
+//   const marker = new google.maps.Marker({
+//     map,
+//     position: place.geometry.location,
+//   });
+// }
 
 function displayRoute(origin,destination,service,display) {
   service
@@ -96,15 +165,15 @@ function computeTotalDistance(result) {
   (document.getElementById("total")).innerHTML = total.toFixed(1) + " miles";
 }
 
-function addMarker(location, map) {
-  // Add the marker at the clicked location, and add the next-available label
-  // from the array of alphabetical characters.
-  new google.maps.Marker({
-    position: location,
-    label: "C",
-    map: map,
-  });
-}
+// function addMarker(location, map) {
+//   // Add the marker at the clicked location, and add the next-available label
+//   // from the array of alphabetical characters.
+//   new google.maps.Marker({
+//     position: location,
+//     label: "C",
+//     map: map,
+//   });
+// }
 
 
   // window.initMap = initMap;
